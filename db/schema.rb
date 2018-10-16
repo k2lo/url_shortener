690b7310f7b18221206d273bf7b6eb8b2340507c
Karol Onaszkiewicz <karol.onaszkiewicz@radgost.com>
2018-10-16 19:53:57 +0200

    pg setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_14_181059) do
+ActiveRecord::Schema.define(version: 2018_10_16_175215) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "links", force: :cascade do |t|
     t.string "og_url"
     t.string "short_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["short_url"], name: "index_links_on_short_url"
   end
 
 end
